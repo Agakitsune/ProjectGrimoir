399ab97ea0c9d4e2a7de0848c8b68c536804794e
Yassin-Said <said.yassin@epitech.eu>
2026-03-15 17:30:33 +0100

    [ADD] - Three skills

---
+++ assets/scripts/fire_ball.gd
@@ -0,0 +1,38 @@
+extends Skill
+@onready var fireball: Sprite2D = $Sprite2D
+@onready var animation_player: AnimationPlayer = $Sprite2D/AnimationPlayer
+
+var hit = false
+var speed = 100
+var random_angle: float
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	randomize()
+	random_angle = deg_to_rad(randf_range(-60, 60))
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if hit == false:
+		launch(delta)
+
+func launch(delta):
+	var dir = direction.rotated(random_angle)
+	
+	fireball.rotation = dir.angle() + PI
+	
+	animation_player.play("fireball_move")
+	fireball.position += dir * speed * delta
+
+func setup(sprite_pos, mouse_pos):
+	direction = (mouse_pos - sprite_pos).normalized()
+	position = sprite_pos
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	hit = true
+	print(area)
+	animation_player.play("fireball_blow")
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "fireball_blow":
+		queue_free()
